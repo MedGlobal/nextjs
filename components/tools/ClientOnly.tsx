84d1c01ab445a42d6440eebe3d91c7a4a0a372bf
Daniel <daniel@foxm.xyz>
2022-08-26 20:32:44 -0500

    Comment

--- components/tools/ClientOnly.tsx
+++ components/tools/ClientOnly.tsx
@@ -4,6 +4,8 @@ interface ClientOnlyProps {
   children: React.ReactNode
 }
 
+// NOTE: Wrap elements with apollo hooks to this one. The hooks are for things that don't need to be
+// crawled by the SEO systems
 export default function ClientOnly({ children }: ClientOnlyProps): JSX.Element {
   const [hasMounted, setHasMounted] = useState(false)
   useEffect(() => {
