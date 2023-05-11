7a9429e9ffa9bbb2b9258af19239b888685effe4
letops <daniel@foxm.xyz>
2023-05-11 15:02:29 -0600

    Add more languages

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'de'],
+    locales: ['en', 'es', 'de', 'fr', 'pt', 'zh'],
   },
   reloadOnPrerender: process.env.NODE_ENV === "development",
 };
